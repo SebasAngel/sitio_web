4595b86cf55985b7857e2a0421337ded5ebddfbe
SebasAngel <122240672+SebasAngel@users.noreply.github.com>
2025-04-20 21:35:31 -0500

    Actualización de estilos, corrección en index.html y eliminación de formulario.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-// alert("Hola Mundo");
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const nameInput = form.querySelector('input[name="name"]');
+    const emailInput = form.querySelector('input[name="email"]');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        // Validar si los campos están vacíos
+        if (nameInput.value.trim() === '' || emailInput.value.trim() === '') {
+            alert('Por favor, completa todos los campos.');
+        } else {
+            alert('Formulario enviado con éxito.');
+        }
+    });
+});
